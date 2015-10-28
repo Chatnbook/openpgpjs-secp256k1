53e1f7234174a1bbd6ac2b27d273dea29ce294a1
Ismael Bejarano <ismael.bejarano@nektra.com>
2015-10-28 16:39:59 -0300

    Unit test for elliptic curve cryptography

--- test/general/index.js
+++ test/general/index.js
@@ -1,6 +1,7 @@
 describe('General', function () {
   require('./basic.js');
   require('./armor.js');
+  require('./ecc.js');
   require('./key.js');
   require('./keyring.js');
   require('./packet.js');
