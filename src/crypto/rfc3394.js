8fd2ad28f18da11eba5d39c4a51e07b004f5fe32
sertony <serkov_anton@mail.ru>
2017-10-03 17:10:06 +0300

    fix rfc3394

--- src/crypto/rfc3394.js
+++ src/crypto/rfc3394.js
@@ -1,71 +1,77 @@
-// Implementation of RFC 3394 Key Wrap & Key Unwrap
+// Implementation of RFC 3394 Key Wrap & Key Unwrap funcions
 
-var AES = require('aes');
+import AES from 'aes';
 
 function createArrayBuffer(data) {
-  var len = data.length;
-  var buff = new ArrayBuffer(len);
-  var view = new Uint8Array(buff);
-  for (var j = 0; j < len; ++j) {
-    view[j] = data.charCodeAt(j);
+  if (typeof data === "string") {
+    var length = data.length;
+    var buffer = new ArrayBuffer(length);
+    var view = new Uint8Array(buffer);
+    for (var j = 0; j < length; ++j) {
+      view[j] = data.charCodeAt(j);
+    }
+    return buffer;
   }
-  return buff;
+  return new Uint8Array(data).buffer;
 }
 
 function unpack(data) {
-  var len = data.length;
-  var buff = createArrayBuffer(data);
-  var view = new DataView(buff);
-  var arr = new Array(len / 8);
-  for (var i = 0; i < len / 8; ++i) {
-    arr[i] = new Uint32Array(2);
-    arr[i][0] = view.getUint32(i * 8);
-    arr[i][1] = view.getUint32(i * 8 + 4);
+  var length = data.length;
+  var buffer = createArrayBuffer(data);
+  var view = new DataView(buffer);
+  var arr = new Uint32Array(length/4);
+  for (var i=0; i<length/4; ++i) {
+    arr[i] = view.getUint32(4*i);
   }
   return arr;
 }
 
-function pack(data) {
-  var len = data.length;
-  var buff = new ArrayBuffer(len * 8);
-  var view = new DataView(buff);
-  for (var i = 0; i < len; ++i) {
-    view.setUint32(i * 8, data[i][0]);
-    view.setUint32(i * 8 + 4, data[i][1]);
+function pack() {
+  var length = 0;
+  for (var k=0; k<arguments.length; ++k) {
+    length += 4*arguments[k].length;
   }
-  return new Uint8Array(buff);
+  var buffer = new ArrayBuffer(length);
+  var view = new DataView(buffer);
+  var offset = 0;
+  for (var i=0; i<arguments.length; ++i) {
+    for (var j=0; j<arguments[i].length; ++j) {
+      view.setUint32(offset+4*j, arguments[i][j]);
+    }
+    offset += 4*arguments[i].length;
+  }
+  return new Uint8Array(buffer);
 }
 
-function createCipher(cipherAlgo, key) {
-  var len = key.length;
-  var buff = createArrayBuffer(key);
-  var view = new DataView(buff);
-  key = new Array(len / 4);
-  for (var i = 0; i < len / 4; ++i) {
-    key[i] = view.getUint32(i * 4);
+function createCipher(key) {
+  var length = key.length;
+  var buffer = createArrayBuffer(key);
+  var view = new DataView(buffer);
+  key = new Array(length/4);
+  for (var i=0; i<length/4; ++i) {
+    key[i] = view.getUint32(4*i);
   }
   return new AES(key);
 }
 
-// RFC 3394 2.2.1 Key Wrap
-function wrap(cipherAlgo, key, data) {
+function wrap(key, data) {
   var IV = new Uint32Array([0xA6A6A6A6, 0xA6A6A6A6]);
-  var aes = createCipher(cipherAlgo, key);
+  var aes = createCipher(key);
   var P = unpack(data);
   var A = IV;
   var R = P;
-  var n = P.length;
+  var n = P.length/2;
   var t = new Uint32Array([0, 0]);
+  var B = new Uint32Array(4);
   for (var j = 0; j <= 5; ++j) {
     for (var i = 0; i < n; ++i) {
       t[1] = n * j + (1 + i);
-      var B = new Uint32Array(4);
       // B = A
       B[0] = A[0];
       B[1] = A[1];
       // B = A || R[i]
-      B[2] = R[i][0];
-      B[3] = R[i][1];
+      B[2] = R[2*i];
+      B[3] = R[2*i+1];
       // B = AES(K, B)
       B = aes.encrypt(B);
       // A = MSB(64, B) ^ t
@@ -73,40 +79,41 @@ function wrap(cipherAlgo, key, data) {
       A[0] = A[0] ^ t[0];
       A[1] = A[1] ^ t[1];
       // R[i] = LSB(64, B)
-      R[i] = B.subarray(2, 4);
+      R[2*i] = B[2];
+      R[2*i+1] = B[3];
     }
   }
-  return pack([A].concat(R));
+  return pack(A, R);
 }
 
-// RFC 3394 2.2.2 Key Unwrap
-function unwrap(cipherAlgo, key, data) {
+function unwrap(key, data) {
   var IV = new Uint32Array([0xA6A6A6A6, 0xA6A6A6A6]);
-  var aes = createCipher(cipherAlgo, key);
+  var aes = createCipher(key);
   var C = unpack(data);
-  var A = C[0];
-  var R = C.slice(1);
-  var n = C.length - 1;
+  var A = C.subarray(0, 2);
+  var R = C.subarray(2);
+  var n = C.length/2-1;
   var t = new Uint32Array([0, 0]);
+  var B = new Uint32Array(4);
   for (var j = 5; j >= 0; --j) {
     for (var i = n - 1; i >= 0; --i) {
       t[1] = n * j + (i + 1);
-      var B = new Uint32Array(4);
       // B = A ^ t
       B[0] = A[0] ^ t[0];
       B[1] = A[1] ^ t[1];
       // B = (A ^ t) || R[i]
-      B[2] = R[i][0];
-      B[3] = R[i][1];
+      B[2] = R[2*i];
+      B[3] = R[2*i+1];
       // B = AES-1(B)
       B = aes.decrypt(B);
       // A = MSB(64, B)
       A = B.subarray(0, 2);
       // R[i] = LSB(64, B)
-      R[i] = B.subarray(2, 4);
+      R[2*i] = B[2];
+      R[2*i+1] = B[3];
     }
   }
-  if (A[0] == IV[0] && A[1] == IV[1]) {
+  if (A[0] === IV[0] && A[1] === IV[1]) {
     return pack(R);
   }
   throw new Error("Key Data Integrity failed");
